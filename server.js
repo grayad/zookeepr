8abc1940fc61d9e50601e7b490973e017e0336e3
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-04 10:42:10 -0400

    validate data

--- server.js
+++ server.js
@@ -79,6 +79,23 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
+// validate new data to ensure key exists and is the right type of data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 // create route for queries
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -106,10 +123,14 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-
-    res.json(req.body);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      // add animal to json file and animals array in this function
+      const animal = createNewAnimal(req.body, animals);
+      res.json(req.body);
+    }
 });
 
 // chain listen() method onto the server to listen for requests
