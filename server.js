5ab30cd8a86bae4a1c1d19b00c34047ff0e33f20
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-03 21:05:15 -0400

    create function to find animal object by ID

--- server.js
+++ server.js
@@ -49,13 +49,30 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-// create route/query
+// takes in the id and array of animals and returns a single animal object
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+// create route for queries
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+// create route to get specific animal (parameter)
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    // if no record exists, send error message
+    } else {
+      res.send(404);
+    }
 });
 
 // chain listen() method onto the server to listen for requests
